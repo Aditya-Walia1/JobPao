004eaac07dd9360a77e0e7b8791b20c6cc9a86ee
Aditya-Walia01 <waliaaditya82@gmail.com>
2023-07-25 00:08:30 +0530

    9th commit

--- app/src/main/java/com/example/jobpao/jobsFragment.java
+++ app/src/main/java/com/example/jobpao/jobsFragment.java
@@ -2,41 +2,131 @@ package com.example.jobpao;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the  factory method to
- * create an instance of this fragment.
- */
+import Model.Data;
+
 public class jobsFragment extends Fragment {
-
-    // Rest of your fragment code
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_jobs, container, false);
 
-        // Initialize the FloatingActionButton
+        // Initialize FirebaseAuth
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+
+        // Initialize the FirebaseDatabase reference
+        DatabaseReference jobPostData = null; // Initialize it properly based on your database structure
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_job_post_id);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
         FloatingActionButton floatingActionButton = view.findViewById(R.id.fabadd);
-        // Set click listener for the FloatingActionButton
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Perform your desired action here when the FloatingActionButton is clicked
 
-                // Create an Intent to go to the NextActivity
+                // Create an Intent to go to the InsertJobPostActivity
                 Intent intent = new Intent(getActivity(), InsertJobPostActivity.class);
                 startActivity(intent);
             }
         });
 
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Data> options =
+                new FirebaseRecyclerOptions.Builder<Data>()
+                        .setQuery(jobPostData, Data.class) // Replace 'jobPostData' with your DatabaseReference for job posts.
+                        .build();
+
+        FirebaseRecyclerAdapter<Data, MyViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Data, MyViewHolder>(options) {
+                    @Override
+
+                    protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull Data model) {
+                        // Bind data to the ViewHolder
+                        holder.setJobTitle(model.getTitle());
+                        holder.setJobDate(model.getDate());
+                        holder.setJobDescription(model.getDescription());
+                        holder.setJobSkills(model.getSkills());
+                        holder.setJobSalary(model.getSalary());
+                    }
+
+                    @NonNull
+                    @Override
+                    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_jobs, parent, false); // Replace 'item_job_post' with your item layout for each job post.
+                        return new MyViewHolder(view);
+                    }
+                };
+
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        View myview;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myview = itemView;
+        }
+
+        public void setJobTitle(String title) {
+            TextView mTitle = myview.findViewById(R.id.job_title);
+            mTitle.setText(title);
+        }
+
+        public void setJobDate(String date) {
+            TextView mDate = myview.findViewById(R.id.job_date);
+            mDate.setText(date);
+        }
+
+        public void setJobDescription(String description) {
+            TextView mDescription = myview.findViewById(R.id.job_description);
+            mDescription.setText(description);
+        }
+
+        public void setJobSkills(String skills) {
+            TextView mskills = myview.findViewById(R.id.job_skill);
+            mskills.setText(skills);
+        }
+
+        public void setJobSalary(String salary) {
+            TextView msalary = myview.findViewById(R.id.job_salary);
+            msalary.setText(salary);
+        }
+
     }
 }
